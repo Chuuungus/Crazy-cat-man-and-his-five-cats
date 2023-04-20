d6a3b59d2926a961a9e202325775aa6851501091
Chuuungus <131351813+Chuuungus@users.noreply.github.com>
2023-04-20 11:05:49 +0100

    Add files via upload
    Uploaded files for website.

---
+++ story.js
@@ -0,0 +1,30 @@
+console.log("hello,chungus");
+
+// variables
+
+var name = ("chungus");
+var age = (39);
+var favcolor = ("black");
+
+// story - DO NOT CHANGE
+
+console.log ("hello " + name);
+
+if (age < 30);
+{
+
+    console.log("youre"+ myage +"? thats an awesome age!");
+    console.log("you're " +  myage + "? wow, you are really old");
+
+}
+
+else if (favcolor == "brown") {
+    console.log(favcolor + ", ha, what adumb color");
+
+}
+else{
+  console.log(favcolor + "is an ok color I guess");
+  
+}
+
+
